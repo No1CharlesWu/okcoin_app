0014d55e4de9d3155d7ffd59818701f24521b283
No1ChalresWu <No1CharlesWu@163.com>
2017-05-04 20:45:09 +0800

    修改部分data_filter 的 ticker。 加了同步和互斥锁。

--- data_filter.py
+++ data_filter.py
@@ -1,8 +1,15 @@
 from collections import namedtuple
+import threading
+import time
+import random
 
 
 class DataFilter(object):
     __instance = None
+    lock_add_ticker = threading.Lock()
+    lock_get_ticker = threading.Lock()
+    con_ticker = threading.Condition()
+    max_ticker_list = 10
 
     def __init__(self):
         self.__ticker_list = list()
@@ -16,12 +23,31 @@ class DataFilter(object):
             DataFilter.__instance = object.__new__(cls, *args, **kwargs)
         return DataFilter.__instance
 
-    def websocket_filter_data(self, data):
-        channel = data['channel']
-        print(channel)
-        if 'ticker' in channel:
+    def ticker_filter(self, data):
+        self.con_ticker.acquire()
+        if len(self.__ticker_list) == self.max_ticker_list:
+            print('max_ticker_list')
+            self.con_ticker.wait()
+            print('ticker_list is not full')
+        self.lock_add_ticker.acquire()
+        try:
+            print(data, type(data))
             self.__ticker_list.append(data['data'])
             self.__ticker_list.sort(key=lambda d: d['timestamp'])
+            print(data['data'])
+        finally:
+            self.lock_add_ticker.release()
+            print('add_ticker_list lock release')
+
+        self.con_ticker.notifyAll()
+        self.con_ticker.release()
+        time.sleep(random.randint(1, 10) * 0.1)
+
+    def websocket_filter_data(self, data):
+        channel = data['channel']
+        if 'ticker' in channel:
+            t = threading.Thread(target=self.ticker_filter, name='ticker_filter', args=(data,))
+            t.start()
         elif 'depth' in channel:
             # TODO
             # self.__depth_list.append(data['data'])
@@ -73,7 +99,24 @@ class DataFilter(object):
             self.__kline_list.append(tmp)
 
     def get_ticker_list(self):
-        return self.__ticker_list
+        self.con_ticker.acquire()
+        if len(self.__ticker_list) == 0:
+            print('ticker_list empty')
+            self.con_ticker.wait()
+            print('ticker_list have something')
+        self.lock_get_ticker.acquire()
+        try:
+            print(self.__ticker_list)
+            r_data = self.__ticker_list.pop()
+            self.__ticker_list.clear()
+            return r_data
+        finally:
+            self.lock_get_ticker.release()
+            print('get_ticker_list lock release')
+
+            self.con_ticker.notifyAll()
+            self.con_ticker.release()
+            time.sleep(random.randint(1, 10) * 0.1)
 
     def get_depth_list_asks(self):
         return self.__depth_list_asks
@@ -94,4 +137,3 @@ global_data_filter = DataFilter()
 
 def get_global_data_filter():
     return global_data_filter
-
